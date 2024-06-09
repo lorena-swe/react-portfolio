17fad2b5a0e01eab6f37942695c018c4d9b0f03a
Lorena <lorenazotaj10@gmail.com>
2024-06-09 22:50:14 +0200

    Small fix on light mode background image

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {
   Box,
   Grid,
   GridItem,
-  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import MainSection from "./components/MainSection";
@@ -12,55 +12,58 @@ import lightThemeImage from "./assets/milad-fakurian-bubble-drop.jpg";
 import theme from "./theme";
 
 function App() {
-  const { colorMode } = useColorMode();
-
   return (
     <ChakraProvider theme={theme}>
-      <Box
-        position="relative"
-        minHeight="100vh"
-        backgroundImage={
-          colorMode === "dark"
-            ? `url(${darkThemeImage})`
-            : `url(${lightThemeImage})`
-        }
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        backgroundSize="cover"
-        backgroundAttachment="fixed"
-        _before={{
-          content: '""',
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          backdropFilter: "blur(100px)",
-          zIndex: 1,
-        }}
-      >
-        <Grid
-          templateAreas={{
-            base: `"main" "nav"`,
-            lg: `"nav main"`, // wider than 1024px
-          }}
-          templateColumns={{
-            base: "1fr",
-            lg: "70px 1fr",
-          }}
-          minHeight="100vh"
-          position="relative"
-          zIndex={2}
-        >
-          <GridItem area="nav">
-            <NavBar />
-          </GridItem>
-          <GridItem area="main">
-            <MainSection />
-          </GridItem>
-        </Grid>
-      </Box>
+      <AppContent />
     </ChakraProvider>
+  );
+}
+
+function AppContent() {
+  // const { colorMode } = useColorMode();
+  const backgroundImage = useColorModeValue(lightThemeImage, darkThemeImage);
+
+  return (
+    <Box
+      position="relative"
+      minHeight="100vh"
+      backgroundImage={`url(${backgroundImage})`}
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundAttachment="fixed"
+      _before={{
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backdropFilter: "blur(100px)",
+        zIndex: 1,
+      }}
+    >
+      <Grid
+        templateAreas={{
+          base: `"main" "nav"`,
+          lg: `"nav main"`, // wider than 1024px
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "70px 1fr",
+        }}
+        minHeight="100vh"
+        position="relative"
+        zIndex={2}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <GridItem area="main">
+          <MainSection />
+        </GridItem>
+      </Grid>
+    </Box>
   );
 }
 
